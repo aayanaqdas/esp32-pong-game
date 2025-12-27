bbe843b0087eb0570962708020ce0eb2bc168a5d
aayanaqdas <aayan.aqdas@outlook.com>
2025-12-27 01:33:52 +0100

    better collision and added CPU logic  so the game is playable.

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,15 @@ const int paddleWidth = 2;
 const int paddleHeight = 16;
 int paddleX = 5;
 int paddleY = 24;
+int cpuPaddleX = 123;
+float cpuPaddleY = 24;
+float cpuSpeed = 0.8;
 
-const int ballSize = 1;
+const float ballSize = 1.5;
 float ballX = 64;
-float ballY = 32;
-float ballSpeedX = 1.0;
-float ballSpeedY = 0.5;
+float ballY = 20;
+float ballSpeedX = 1.3;
+float ballSpeedY = 0.8;
 
 void setup()
 {
@@ -37,41 +40,67 @@ void setup()
 void loop()
 {
   paddleY = map(analogRead(potPin), 0, 4095, 0, SCREEN_HEIGHT - paddleHeight);
+  paddleY = constrain(paddleY, 0, SCREEN_HEIGHT - paddleHeight);
 
   ballX += ballSpeedX;
   ballY += ballSpeedY;
 
-  // Check if the ball hit the end of the screen then bounce it back
+  // Only move when ball is on the right of the screen
+  if (ballX >= (SCREEN_WIDTH / 2))
+  {
+    // Track ball center vs paddle center
+    float cpuPaddleCenter = cpuPaddleY + (paddleHeight / 2);
+    if (ballY > cpuPaddleCenter)
+    {
+      cpuPaddleY += 0.8; // move down
+    }
+    else if (ballY < cpuPaddleCenter)
+    {
+      cpuPaddleY -= 0.8; // move up
+    }
+  }
+  cpuPaddleY = constrain(cpuPaddleY, 0, SCREEN_HEIGHT - paddleHeight);
+
+  // Wall collision Y bounce ball back
   if (ballY <= 0 || ballY >= SCREEN_HEIGHT)
   {
     ballSpeedY *= -1;
   }
-  else if (ballX >= SCREEN_WIDTH)
-  {
-    ballSpeedX *= -1;
-  }
 
-  // Check paddle x
-  if (ballX - ballSize == paddleX + paddleWidth)
+  // Player paddle collision
+  if (ballX - ballSize <= paddleX + paddleWidth)
   {
     // Check if ball Y is between the paddle
     if (ballY >= paddleY && ballY <= paddleY + paddleHeight)
     {
-      ballSpeedX *= -1;
+      ballSpeedX *= -1.1;
       ballX = paddleX + paddleWidth + ballSize; // Push the ball off the paddle
     }
   }
-  else if (ballX <= 0)
+
+  // CPU paddle collision
+  if (ballX + ballSize >= cpuPaddleX && ballX < cpuPaddleX + paddleWidth)
+  {
+    if (ballY >= cpuPaddleY && ballY <= cpuPaddleY + paddleHeight)
+    {
+      ballSpeedX *= -1;
+      ballX = cpuPaddleX - ballSize;
+    }
+  }
+
+  // Wall collision X Reset ball
+  if (ballX <= 0 || ballX >= SCREEN_WIDTH)
   {
     ballX = 64;
     ballY = 20;
-    ballSpeedX = 1.0;
-    ballSpeedY = 0.5;
+    ballSpeedX = 1.3;
+    ballSpeedY = 0.8;
   }
 
   display.clearDisplay();
 
   display.fillRect(paddleX, paddleY, paddleWidth, paddleHeight, 1);
+  display.fillRect(cpuPaddleX, cpuPaddleY, paddleWidth, paddleHeight, 1);
   display.fillCircle(ballX, ballY, ballSize, 1);
 
   display.display();
