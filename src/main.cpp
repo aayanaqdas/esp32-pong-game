0140d38e7f30d717b4b46979c87ecb53ec3a2e59
aayanaqdas <aayan.aqdas@outlook.com>
2025-12-29 03:30:40 +0100

    Scores

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,55 @@ const int potPin = 14;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const int paddleWidth = 2;
-const int paddleHeight = 16;
-int paddleX = 5;
+const int paddleWidth = 3;
+const int paddleHeight = 12;
+int paddleX = 8;
 int paddleY = 24;
 int cpuPaddleX = 123;
 float cpuPaddleY = 24;
 float cpuSpeed = 0.8;
 
-const float ballSize = 1.5;
+int playerScore = 0;
+int cpuScore = 0;
+
+const float ballSize = 1;
 float ballX = 64;
 float ballY = 20;
-float ballSpeedX = 1.3;
-float ballSpeedY = 0.8;
+float ballSpeedX = 1.6;
+float ballSpeedY = 1.1;
+
+void drawGame()
+{
+  display.clearDisplay();
+  display.fillRect(paddleX, paddleY, paddleWidth, paddleHeight, 1);
+  display.fillRect(cpuPaddleX, cpuPaddleY, paddleWidth, paddleHeight, 1);
+  display.fillCircle(ballX, ballY, ballSize, 1);
+
+  for (int i = 2; i < SCREEN_HEIGHT - 2; i += 8)
+  {
+    display.fillRect((SCREEN_WIDTH / 2) - 1, i, 2, 4, 1);
+  }
+
+  display.setTextColor(1);
+  display.setTextSize(2);
+  display.setCursor(SCREEN_WIDTH / 2 - 20, 2);
+  display.print(playerScore);
+
+  display.setCursor(SCREEN_WIDTH / 2 + 10, 2);
+  display.print(cpuScore);
+
+  display.display();
+}
+
+void resetBall(int loser)
+{
+  ballX = 64;
+  ballY = 20;
+
+  int direction = (loser == 0) ? 1 : -1;
+  ballSpeedX = direction * 1.6;
+  ballSpeedY = (random(0, 2) == 0 ? 1 : -1) * 1.1;
+}
 
 void setup()
 {
@@ -73,7 +109,7 @@ void loop()
     // Check if ball Y is between the paddle
     if (ballY >= paddleY && ballY <= paddleY + paddleHeight)
     {
-      ballSpeedX *= -1.1;
+      ballSpeedX *= -1.2;
       ballX = paddleX + paddleWidth + ballSize; // Push the ball off the paddle
     }
   }
@@ -88,20 +124,16 @@ void loop()
     }
   }
 
-  // Wall collision X Reset ball
-  if (ballX <= 0 || ballX >= SCREEN_WIDTH)
+  if (ballX <= 0)
   {
-    ballX = 64;
-    ballY = 20;
-    ballSpeedX = 1.3;
-    ballSpeedY = 0.8;
+    cpuScore++;
+    resetBall(0);
+  }
+  else if (ballX >= SCREEN_WIDTH)
+  {
+    playerScore++;
+    resetBall(1);
   }
 
-  display.clearDisplay();
-
-  display.fillRect(paddleX, paddleY, paddleWidth, paddleHeight, 1);
-  display.fillRect(cpuPaddleX, cpuPaddleY, paddleWidth, paddleHeight, 1);
-  display.fillCircle(ballX, ballY, ballSize, 1);
-
-  display.display();
+  drawGame();
 }
